0724f9781b2306db566095dac04862ed19ddd7fb
ivan-marinovic <ivan.marinovic777@gmail.com>
2023-03-01 17:12:22 +0100

    discovery-server created

--- inventory-service/src/main/java/com/ivan/inventory/InventoryApplication.java
+++ inventory-service/src/main/java/com/ivan/inventory/InventoryApplication.java
@@ -5,9 +5,11 @@ import com.ivan.inventory.repository.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InventoryApplication {
     public static void main(String[] args) {
         SpringApplication.run(InventoryApplication.class, args);
